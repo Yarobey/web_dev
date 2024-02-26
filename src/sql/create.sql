234a99b205bc1128956147cb76e33a53733dcd29
Egor Gikalo <gikalo.egor@gmail.com>
2024-02-26 23:01:22 +0300

    sql scripts added

---
+++ src/sql/create.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS Customer CASCADE;
+CREATE TABLE Customer (
+  Customer_ID SERIAL PRIMARY KEY,
+  Name        text NOT NULL,
+  Phone       char(20),
+  email       text,
+  Address     text,
+  City        text,
+  zip_code    char(10),
+  Description text
+);
+
+DROP TABLE IF EXISTS Order_ CASCADE;
+CREATE TABLE Order_ (
+  Order_number  SERIAL PRIMARY KEY,
+  Customer_ID   int NOT NULL REFERENCES Customer(Customer_ID) ON DELETE CASCADE,
+  Ship_date     DATE NOT NULL,
+  Comment       text
+);
+
+DROP TABLE IF EXISTS Product CASCADE;
+CREATE TABLE Product (
+  Product_ID      SERIAL PRIMARY KEY,
+  Name            text NOT NULL,
+  Product_type    text,
+  size1           int,
+  size2           int,
+  size3           int,
+  Description     text,
+  Expiration_date date
+);
+
+DROP TABLE IF EXISTS Supplier CASCADE;
+CREATE TABLE Supplier (
+  Supplier_ID SERIAL PRIMARY KEY,
+  Name        text NOT NULL,
+  Phone       char(20),
+  email       text,
+  Address     text,
+  Description text
+);
+
+DROP TABLE IF EXISTS Supply CASCADE;
+CREATE TABLE Supply (
+  Supply_ID SERIAL PRIMARY KEY,
+  Supplier_ID   int REFERENCES Supplier(Supplier_ID) ON DELETE CASCADE,
+  Ship_date     date
+);
+
+DROP TABLE IF EXISTS Stock CASCADE;
+CREATE TABLE Stock (
+  Stock_ID    SERIAL PRIMARY KEY,
+  Product_ID  int NOT NULL REFERENCES Product(Product_ID) ON DELETE CASCADE,
+  Supply_ID   int NOT NULL REFERENCES Supply(Supply_ID) ON DELETE CASCADE,
+  Quantity    int CHECK(Quantity >= 0)
+);
+
+
+DROP TABLE IF EXISTS Warehouse_space CASCADE;
+CREATE TABLE Warehouse_space (
+  Place_ID      SERIAL PRIMARY KEY,
+  Stock_ID      int NOT NULL REFERENCES Stock(Stock_ID),
+  Room          int,
+  Shelf         int,
+  Product_type  text,
+  Empty_shelf   bool
+):
+
+DROP TABLE IF EXISTS Goods CASCADE;
+CREATE TABLE Goods ( 
+  Item_ID       SERIAL PRIMARY KEY,
+  Stock_ID      int NOT NULL REFERENCES Stock(Stock_ID),
+  Order_number  int NOT NULL REFERENCES Order_(Order_number) ON DELETE CASCADE,
+  Quantity      int CHECK(Quantity > 0)
+);
+
---
+++ src/sql/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO Customer (Name, Phone, email) VALUES
+  ('Tim Cook', '8(926)111-11-11', 'cook@apple.com'),
+  ('Kenichiro Yoshida', '8(926)222-22-22', 'yoshida@sony.com')
+  ('Jensen Huang', '8(926)222-22-22', 'huang@nvidia.com'),
+
+
+INSERT INTO Order_ (Customer_ID, Ship_date, Comment) VALUES
+  (1, '2023-11-01', 'to tim cook'),
+  (2, '2023-11-02', 'to Kenichiro Yoshida'),
+  (3, '2023-11-03', 'to Jensen Huang')
+  
+
+INSERT INTO Product (Name, Product_type) VALUES
+  ('MacBook', 'electronics'),
+  ('Haribo', 'food'),
+  ('Millenium Falcon', 'toys');
+
+INSERT INTO Supplier (Name, Phone, email, Address, Description) VALUES
+  ('Apple', '8(800)111-11-11', 'apple@gmail.com', 'Cupertino', 'you know this company'),
+  ('Haribo', '8(800)222-22-22', 'haribo@gmail.com', 'Berlin', 'im pretty sure that youve eaten them at least once'),
+  ('Lego', '8(800)333-33-33', 'lego@gmail.com', 'Copenhagen', 'you should buy their shares');
+
+INSERT INTO Supply (Supplier_ID, Ship_date) VALUES
+  (1, '2023-01-01'),
+  (2, '2023-01-02'),
+  (3, '2023-11-03');
+
+
+INSERT INTO Stock (Product_ID, Supply_ID, Quantity) VALUES
+  (1, 1, 5),
+  (2, 2, 5),
+  (3, 3, 5);
+
+
+INSERT INTO Warehouse_space(Stock_ID, Room, Shelf, Product_type, Empty_shelf) VALUES
+  (1, 1, 1, 'electronics', true),
+  (2, 2, 2, 'food', true),
+  (3, 3, 3, 'toys', true);
+
+
+INSERT INTO Goods (Stock_ID, Order_number, Quantity) VALUES
+  (1, 20, 5),
+  (2, 19, 5),
+  (3, 18, 5);
+
+
+
