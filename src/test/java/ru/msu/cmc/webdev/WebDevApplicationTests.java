45ab0d95a8ee4e686254b6cd40700ff88f6f5230
Egor Gikalo <gikalo.egor@gmail.com>
2024-03-24 17:43:38 +0300

    project: initial version

---
+++ src/test/java/ru/msu/cmc/webdev/WebDevApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.webdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
